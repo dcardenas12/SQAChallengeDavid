bcc0b779f73309a8bdef64505fc011e128281299
David Cardenas Guzman <david.cardenas@Davids-MacBook-Pro.local>
2021-10-20 09:12:43 -0500

    changed services to validate Jenkins job

--- wdio.conf.js
+++ wdio.conf.js
@@ -124,7 +124,7 @@ exports.config = {
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
     services: [
-        process.env.LOCAL_SERVICE
+        'chromedriver'
     ],
     
     // Framework you want to run your specs with.
