4cc2fb1b32f3a116cecbe11906b00d33e680e517
David Cardenas Guzman <david.cardenas@Davids-MacBook-Pro.local>
2021-10-21 09:45:37 -0500

    improvement on how to define execution environment

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,4 +1,8 @@
 require('dotenv').config()
+
+const myServices = new Map()
+myServices.set('LOCAL','chromedriver')
+myServices.set('REMOTE',"['browserstack', {browserstackLocal: true}]")
 exports.config = {
     //
     // ====================
@@ -123,10 +127,8 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: [
-        process.env.BROWSERSTACK_SERVICE
-    ],
-    
+    services: [myServices.get(process.env.SERVICE)],
+
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
     // see also: https://webdriver.io/docs/frameworks
